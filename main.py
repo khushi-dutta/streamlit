63a883ba4bd13b39b5914d4c6a3c90f23121e585
Khushi Dutta <145875178+khushi-dutta@users.noreply.github.com>
2024-09-22 21:27:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import os
+from PIL import Image
+import numpy as np
+import pickle
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from sklearn.neighbors import NearestNeighbors
+from numpy.linalg import norm
+
+# Load feature list and filenames
+feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
+filenames = pickle.load(open('filenames.pkl', 'rb'))
+
+# Load the model
+model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+st.markdown("<h1 style='color: #ae72cf;'>Get What You Want</h1>", unsafe_allow_html=True)
+
+def save_uploaded_file(uploaded_file):
+    try:
+        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+        return True
+    except Exception as e:
+        st.error(f"Error saving file: {e}")
+        return False
+
+def feature_extraction(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    result = model.predict(preprocessed_img).flatten()
+    normalized_result = result / norm(result)
+    return normalized_result
+
+def recommend(features, feature_list):
+    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+    neighbors.fit(feature_list)
+    distances, indices = neighbors.kneighbors([features])
+    return indices
+
+# File upload
+uploaded_file = st.file_uploader("Choose an image")
+if uploaded_file is not None:
+    if save_uploaded_file(uploaded_file):
+        # Display the file
+        display_image = Image.open(uploaded_file)
+        st.image(display_image)
+        
+        # Feature extraction
+        features = feature_extraction(os.path.join("uploads", uploaded_file.name), model)
+        
+        # Recommendations
+        indices = recommend(features, feature_list)
+        
+        # Show recommendations
+        cols = st.columns(5)
+        for i, col in enumerate(cols):
+            with col:
+                st.image(filenames[indices[0][i]], use_column_width=True)
+
+    else:
+        st.header("Some error occurred in file upload")
